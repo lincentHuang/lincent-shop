3ab804ba9b3d9b44fe27ac20c3b1ee83ae787e56
lincent <lincent.js.tw@tripleonetech.net>
2025-11-04 16:02:30 +0800

    update provider for google & facebook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+"use client";
+
 import Image from "next/image";
 import "../styles/test.scss";
 import { HomeView } from "@/views/home";
-import axios from "axios";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session?.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  );
+
   return (
     <div className="flex items-center justify-center font-sans dark:bg-black">
       <HomeView />
